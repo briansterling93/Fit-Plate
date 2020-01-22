ac0b07cadac830e4c3e0da3b799ea9ca76396761
Brian <briansterling93@yahoo.com>
2020-01-22 00:44:57 -0700

    styling changes

--- src/components/FoodQuerySection.js
+++ src/components/FoodQuerySection.js
@@ -2,24 +2,44 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const FoodQuerySection = () => {
+  const [foodQuery, setFoodQuery] = useState("");
   const [foodItem, setFoodItem] = useState([]);
 
+  //User input food query
+  const querySearch = e => {
+    setFoodQuery(e.target.value);
+  };
+
+  //Query UI Input
   const queryFood = async e => {
     e.preventDefault();
+
+    const query = foodQuery;
+
+    const newSearch = {
+      generalSearchInput: query
+    };
+
     try {
       const headers = {
         "Content-Type": "application/json"
       };
 
-      const res = await axios.get(
-        "https://api.nal.usda.gov/fdc/v1/search?api_key=Nqr7rveC0s3PtsDv3yNzxBa8v6TdKsifRah4by2v"
+      const body = JSON.stringify(newSearch);
+
+      const res = await axios.post(
+        "https://api.nal.usda.gov/fdc/v1/search?api_key=Nqr7rveC0s3PtsDv3yNzxBa8v6TdKsifRah4by2v",
+        body,
+        headers
       );
 
-      const item = JSON.stringify(res.data.foods[1].description);
+      //   const res = await axios.get(
+      //     "https://api.nal.usda.gov/fdc/v1/search?api_key=Nqr7rveC0s3PtsDv3yNzxBa8v6TdKsifRah4by2v",
+      //     body,
+      //     headers
+      //   );
 
-      await setFoodItem(item);
-
-      //   setFoodItem(res.data.foods[1]);
+      console.log(res.data);
     } catch (error) {
       console.error(error.message);
     }
@@ -27,6 +47,7 @@ const FoodQuerySection = () => {
 
   return (
     <div>
+      <input name="food-search" value={foodQuery} onChange={querySearch} />
       <button onClick={queryFood}>Search</button>
       <div>{foodItem}</div>
     </div>
